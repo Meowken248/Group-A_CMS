061b7d78bf4bdfb1f6989deef2c5cc245d9ab734
kaido <leanhtuan02072006@gmail.com>
2026-09-17 09:20:08 +0700

    feat(module-13): hoan thanh module 13 Pages dang cot dung responsive theo yeu cau GV

---
+++ wp-content/themes/NhomA_CMS_15module/page-module-13.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template Name: Module 13 - Pages (Trang đào tạo)
+ * Chức năng: Template chuyên biệt để hiển thị và kiểm tra Module 13
+ */
+
+get_header();
+
+// Nạp file Module 13 từ thư mục 13
+include get_template_directory() . '/13/test.php';
+
+get_footer();
